d06de187ace05a972467f2b3a74c8cc1764b837c
Aristokrat007Z <novitskyi21@icloud.com>
2024-11-14 22:03:49 +0300

    add config auth firebase

---
+++ src/config/auth/index.tsx
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyAtJc35dPuFOcCFh3_fFDCZFewN56AzqV8',
+	authDomain: 'auth-router-24e51.firebaseapp.com',
+	projectId: 'auth-router-24e51',
+	storageBucket: 'auth-router-24e51.firebasestorage.app',
+	messagingSenderId: '1048069694137',
+	appId: '1:1048069694137:web:85aa49ae561e1317367168',
+	measurementId: 'G-Y085NC1G8S',
+}
+
+export const app = initializeApp(firebaseConfig)
